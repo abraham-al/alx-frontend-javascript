b02e5894e41015d90040ce5c23599fa3270b4698
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2023-01-03 11:43:10 +0300

    update

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,9 @@
 export default function getListStudents() {
-  return [
+  const arr1 = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+
+  return arr1;
 }
